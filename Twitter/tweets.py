03983a5fcaabc590f22a70aef2a8e93645b28006
brian02oriel <brian.nieto1@utp.ac.pa>
2020-06-29 10:48:24 -0500

    setting env

--- Twitter/tweets.py
+++ Twitter/tweets.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from os import environ
 import tweepy as tw
 import pandas as pd
 import json
@@ -6,10 +7,10 @@ from Twitter.data_management import Summary
 
 def Auth():
     #API keys
-    consumer_key= 'ZkaLWawtOntqbTgtcPHlKrO1j'
-    consumer_secret= 'vVzxBCkKRY3iNCASZnbxwAyXMgISI9doZmYS8M8DUcRl7wVZCh'
-    access_token= '1017240891197657094-D5l0VSWNeRBIRamwNIsioY2JMuz8oJ'
-    access_token_secret= 'UXRPR0iwIlpw0QQFStvP4sNu40Gmu6C6OSwlFaO8dUjIU'
+    consumer_key= environ.get('CONSUMER_KEY')
+    consumer_secret= environ.get('CONSUMER_SECRET')
+    access_token= environ.get('ACCESS_TOKEN')
+    access_token_secret= environ.get('ACCESS_SECRET')
 
     #Authentication 
     auth = tw.OAuthHandler(consumer_key, consumer_secret)
@@ -26,7 +27,7 @@ def Tweets(keywords):
     date_since = "2019-06-01"
 
     #Collect tweets
-    tweets = tw.Cursor(api.search, q=search_words, lang="es", since=date_since).items(250)
+    tweets = tw.Cursor(api.search, q=search_words, lang="es", since=date_since).items(int(environ.get('DEVELOPMENT_TWEETS_COUNT')))
 
     #Iterates on tweet
     users_locs = [[tweet.user.screen_name, tweet.text.encode('utf-8'), tweet.user.location , tweet.created_at] for tweet in tweets]
